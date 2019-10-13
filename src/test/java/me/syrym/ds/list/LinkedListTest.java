ee1d45297921288d88c9f65b4d3a10f5f4e7a1ed
Syrym <justsyrym@gmail.com>
2019-10-13 21:03:37 +0300

    implement linkedlist with tests

---
+++ src/test/java/me/syrym/ds/list/LinkedListTest.java
@@ -0,0 +1,48 @@
+package me.syrym.ds.list;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LinkedListTest {
+    private LinkedList<Integer> list;
+
+    @Before
+    public void setup() {
+        list = new LinkedList<Integer>();
+    }
+
+    @Test
+    public void addFirstTest_ifHeadIsNull() {
+        list.addFirst(1);
+        Assert.assertEquals(1, list.getSize());
+        Assert.assertEquals(new Integer(1), list.getFirst());
+    }
+
+    @Test
+    public void addLastTest_ifHeadIsNull() {
+        list.addLast(1);
+        Assert.assertEquals(1, list.getSize());
+        Assert.assertEquals(new Integer(1), list.getLast());
+    }
+
+    @Test
+    public void addFirstTest_ifHeadIsNotNull() {
+        list.addFirst(1);
+        list.addFirst(2);
+        list.addFirst(3);
+        Assert.assertEquals(3, list.getSize());
+        Assert.assertEquals(new Integer(1), list.getLast());
+        Assert.assertEquals(new Integer(3), list.getFirst());
+    }
+
+    @Test
+    public void addLastTest_ifHeadIsNotNull() {
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        Assert.assertEquals(3, list.getSize());
+        Assert.assertEquals(new Integer(1), list.getFirst());
+        Assert.assertEquals(new Integer(3), list.getLast());
+    }
+}
